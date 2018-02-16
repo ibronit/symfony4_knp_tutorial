6026db07f58ee22d0f28214ca8ba94ddfa0b8293
Parraghy Norbert <parraghy.norbert@netpositive.hu>
2018-02-16 16:23:18 +0100

    Changes parent of the ArticleController to AbstractController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController extends Controller
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
